34bdf6b1137e44053fe54d4f2a695b6da645a9e7
Julien Truffaut <truffaut.julien@gmail.com>
2025-09-29 21:57:56 +0200

    save dofus db items locally

--- src/dofus_db_models.rs
+++ src/dofus_db_models.rs
@@ -7,7 +7,7 @@ pub struct GetObjectsResponse {
     pub total: u32,
     pub limit: u32,
     pub skip: u32,
-    pub data: Vec<DofusDbObject>
+    pub data: Vec<serde_json::Value>
 }
 
 #[derive(Debug, Deserialize)]
