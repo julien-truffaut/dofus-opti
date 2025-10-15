52df86a0bac0ec04d400a1e3d4feba1053eeb1fc
Julien Truffaut <truffaut.julien@gmail.com>
2025-10-15 10:07:55 +0200

    get rid of skip counter

--- src/dofus_db_client.rs
+++ src/dofus_db_client.rs
@@ -3,24 +3,21 @@ use crate::{dofus_db_parser::gear_type_to_code};
 
 pub async fn fetch_all_gears(gear_type: &GearType) -> reqwest::Result<Vec<serde_json::Value>> {
     let mut gears: Vec<serde_json::Value> = vec!();
-    let mut skip = 0;
 
     loop {
-        println!("Fetching {gear_type} from {skip}");
-        let response = fetch_gear(gear_type, skip).await?;
+        let mut response = fetch_gear(gear_type, gears.len()).await?;
         println!("Received {} {gear_type}", response.data.len());
         if response.data.is_empty() {
             break;
         } else {
-            skip += response.data.len() as u32;
-            gears.extend(response.data);
+            gears.append(&mut response.data);
         }
     }
 
     Ok(gears)
 }
 
-pub async fn fetch_gear(gear_type: &GearType, skip: u32) -> reqwest::Result<GetObjectsResponse> {
+pub async fn fetch_gear(gear_type: &GearType, skip: usize) -> reqwest::Result<GetObjectsResponse> {
     let gear_code = gear_type_to_code(gear_type);
     let url = format!("https://api.dofusdb.fr/items?typeId[$in][]={}&$sort=-id&$skip={}", gear_code, skip);
 
